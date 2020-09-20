d23a1e31e24a031aa18786f55558b691879a1228
Bryan Palmer <bryanpalmer@Bryans-Mac-mini.lan>
2020-09-20 10:30:18 +1000

    Add alternate approach to calculation

--- gini.py
+++ gini.py
@@ -5,9 +5,7 @@ def gini(x):
     """Calculate the Gini coefficient for series of observed values (x). 
     where:
     * x is the array of observed values (and all x are positive, non-zero values)
-      Note: x should be a numpy array or a pandas series
-    * n is the number of values observed (derived from x)
-    * i is the rank of the x-values when sorted in ascending order (derived from x)
+      Note: x must be a numpy array or a pandas series
     See: https://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
     """
     
@@ -24,11 +22,36 @@ def gini(x):
     # sort values in ascending order
     x = np.sort(x)
 
-    # get n
+    # n is the number of values observed
     n = len(x)
         
-    # i: numpy array from 1 to n inclusive
+    # i is the rank of the x-values when sorted in ascending order
     i = np.arange(1, n+1)
     
     # - calculate the Gini coefficient
     return ((2 * i - n - 1) * x).sum() / (n * x.sum())
+    
+    
+# An alternate approach ...
+def gini2(x):
+    """ Calculate Gini from observed values using summation-as-integration """
+    
+    if not isinstance(x, pd.Series) and not isinstance(x, np.ndarray):
+        raise TypeError('input series must be a pandas Series or a numpy array')
+    if x.min() <= 0:
+        raise ValueError('all input values in series must be positive and non-zero')
+
+    # let's work with numpy arrays
+    if isinstance(x, pd.Series):
+        x = x.to_numpy()
+    
+    # calculate the line-of-equality and the lorenz curve
+    lorenz = np.sort(x).cumsum()
+    height = lorenz[-1]
+    n = len(lorenz)
+    equality = np.arange(1, n+1) * height / n
+    
+    # calculate Gini
+    A_area = (equality - lorenz).sum() # area between lorenz and equality
+    AB_area = n * height / 2 # area of triangle
+    return A_area / AB_area
