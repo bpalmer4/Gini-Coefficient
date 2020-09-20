00242a379608923c913c0a48cac467b02f8e3726
Bryan Palmer <bryanpalmer@Bryans-Mac-mini.lan>
2020-09-20 10:22:02 +1000

    Tidy-ups

--- gini.py
+++ gini.py
@@ -1,22 +1,34 @@
 import pandas as pd
 import numpy as np
 
-def gini(x: pd.Series)-> float:
-    """Calculate the Gini coefficient for pandas Series x, where all x are positive"""
+def gini(x):
+    """Calculate the Gini coefficient for series of observed values (x). 
+    where:
+    * x is the array of observed values (and all x are positive, non-zero values)
+      Note: x should be a numpy array or a pandas series
+    * n is the number of values observed (derived from x)
+    * i is the rank of the x-values when sorted in ascending order (derived from x)
+    See: https://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
+    """
     
     # - sanity checks
-    if not isinstance(x, pd.Series):
-        raise TypeError('input series must be a pandas Series')
-    if x.min() < 0:
-        raise ValueError('all input values in series must be zero or positive')
-    if x.sum() <= 0:
-        raise ValueError('the sum of the input series must be greater than zero')
+    if not isinstance(x, pd.Series) and not isinstance(x, np.ndarray):
+        raise TypeError('input series must be a pandas Series or a numpy ndarray')
+    if x.min() <= 0:
+        raise ValueError('all input values in series must be positive and non-zero')
 
-    # - preparation for calculation
+    # let's work with numpy arrays
+    if isinstance(x, pd.Series):
+        x = x.to_numpy()
+        
     # sort values in ascending order
-    x = x.sort_values(ascending=True)
-    # i: numpy array from 1 to n
-    i = np.arange(1, x.size+1)
+    x = np.sort(x)
+
+    # get n
+    n = len(x)
+        
+    # i: numpy array from 1 to n inclusive
+    i = np.arange(1, n+1)
     
     # - calculate the Gini coefficient
-    return ((2 * i - x.size - 1) * x).sum() / (x.size * x.sum())
+    return ((2 * i - n - 1) * x).sum() / (n * x.sum())
